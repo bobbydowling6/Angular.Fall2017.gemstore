97513cbb1a479ba1a2237934140ba52bcedcb174
bobbydowling6 <bobbydowling6@gmail.com>
2017-10-25 12:35:13 -0500

    modified files

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-var gems = [{
-name: 'Dodecahedron',
-price: 2.95,
-description: 'Some gems have hidden qualities beyond their lustre, beyond their shine... Dodecahedron is one of those gems',
-canPurchase: true,
-outOfStock: false,
-image: 'images/gem-01.gif',
-reviews: [
+//For now, just hard-coding a model!
+var gems = [
     {
-        stars: 5,
-        author: 'josephj@codingtemple',
-        body: 'I love this product!'
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: 'Some gems have hidden qualities beyond their lustre, beyond their shine... Dodecahedron is one of those gems',
+        canPurchase: true,
+        outOfStock: false,
+        image: 'images/gem-01.gif',
+        reviews: [
+            {
+                stars: 2,
+                author: 'ripalp@codingtemple',
+                body: 'I hate this product!'
+            },
+            {
+                stars: 5,
+                author: 'josephj@codingtemple',
+                body: 'I love this product!'
+            }
+        ]
     }, {
-        stars: 2,
-        author: 'ripalr@codingtemple',
-        body: 'I hate this product'
-    }
-]
-}, {
         name: "Pentagonal Gem",
         price: 5.95,
         description: "The origin of the pentagonal gem is unknown.",
         canPurchase: true,
         outOfStock: true,
         image: 'images/gem-02.gif'
-    }, {
+     }, {
+
         name: "Triangular Gem",
         price: 3.95,
         description: "The origin of the Triangular gem is that is's over 5,000 years old.",
@@ -31,6 +35,7 @@ reviews: [
         outOfStock: false,
         image: 'images/gem-03.gif'
     }, {
+
         name: "Triangular Gem",
         price: 3.95,
         description: "The origin of the Triangular gem is that it's over 5,000 years old.",
@@ -38,6 +43,7 @@ reviews: [
         outOfStock: false,
         image: 'images/gem-03.gif'
     }, {
+
         name: "Tricubal Gem",
         price: 6.95,
         description: "The origin of the Tricubal gem is that it's over 3,000 years old.",
@@ -45,6 +51,7 @@ reviews: [
         outOfStock: true,
         image: 'images/gem-04.gif'
     }, {
+
         name: "Cubal Gem",
         price: 7.95,
         description: "The origin of the Cubal gem is that it's over 5,000 years old.",
@@ -53,21 +60,21 @@ reviews: [
         image: 'images/gem-05.gif'
     }
 ];
-
-//Module declaration - I'm creating a new custom module called "myApp" which I can now use on the page. If I pass one argument to the module method, I'm "fetching" an existing module. If I pass two arguments to the module method, I'm creating a new module. In this instance, I'm saving the app module to a variable as I'll reuse it often throughout the page
+//Module declaration - I'm creating a new custom module called "myApp" which I can now use on the page. If I pass one argument to the module method, I'm "Fetching" an existing module. If I pass two arguments to the module method, I'm creating a new module.  In this instance, I'm saving the app module to a variable as I'll reuse it often throughout the page
 var app = angular.module("myApp", []);
 //Once I have a reference to the app module, I can add other services, controllers, filters, and directives to it using built-in functions.  In this case, passing two arguments to the controller method will create a new controller
 app.controller("myController", function ($scope) {
     $scope.Initialized = function () {
         $scope.models = gems;
     }
+
 });
 
-app.directive("storePanels", function() {
+app.directive("storePanels", function () {
     var directiveObject = {
         templateUrl: "templates/store-panels.html",
-        controller: function($scope){
-            $scope.SelectTab = function(tab){
+        controller: function ($scope) {
+            $scope.SelectTab = function (tab) {
                 $scope.tab = tab;
             }
         }
@@ -75,24 +82,58 @@ app.directive("storePanels", function() {
     return directiveObject;
 })
 
-app.directive("storeReviews", function(){
-    var directObject = {
+app.directive("storeReviews", function () {
+    var directiveObject = {
         templateUrl: "templates/store-reviews.html",
-        controller: function($scope) {
-            $scope.AddReview = function(product) {
-               //Just to be safe, make sure to add a reviews array if it doesn't already exist
-		if(!product.reviews){
-			product.reviews = [];
-		}
-		product.reviews.push($scope.newReview);
+        controller: function ($scope) {
+            $scope.AddReview = function (product) {
+                //Just to be safe, make sure to add a reviews array if it doesn't already exist
+                if (!product.reviews) {
+                    product.reviews = [];
+                }
+                product.reviews.push($scope.newReview);
 
-            //This resets the review
-            $scope.newReview = {};
+                //This resets the review
+                $scope.newReview = {};
+
+                $scope.reviewForm.$setPristine();
+            }
+
+            $scope.StarsInvalid = function () {
+                return $scope.reviewForm.stars.$invalid && $scope.reviewForm.stars.$dirty
+            }
+
+            $scope.BodyInvalid = function () {
+                return $scope.reviewForm.body.$invalid && $scope.reviewForm.body.$dirty
+            }
+
+            $scope.AuthorInvalid = function () {
+                return $scope.reviewForm.author.$invalid && $scope.reviewForm.author.$dirty
+            }
+
+            $scope.FormInvalid = function () {
+                return $scope.StarsInvalid() || $scope.BodyInvalid() || $scope.AuthorInvalid();
             }
         }
     };
-    return directObject;
+    return directiveObject;
 })
+
+
+//I can also attach directives to modules:
+//app.directive("myDirective", function(){
+//    var directiveObject = {
+//        template: "<div>This is a template directive.</div>"
+//    };
+//    return directiveObject;
+//});
+//A better template directive
+//app.directive("myOtherDirective", function(){
+//    var directiveObject = {
+//        templateUrl: "template.html"
+//    };
+//    return directiveObject;
+//});
 
 //app.directive("myDirective", function (){
 //var directObject = {
